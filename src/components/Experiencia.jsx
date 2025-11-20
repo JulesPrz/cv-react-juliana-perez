4da08b006755f201f34332d50e4156f137e53a5d
JulesPrz <julisperez.ch@gmail.com>
2025-11-20 16:13:38 -0500

    feat: componente Experiencia con 4 cargos definidos

---
+++ src/components/Experiencia.jsx
@@ -0,0 +1,21 @@
+export default function Experiencia() {
+  return (
+    <section>
+      <h3>Experiencia Profesional</h3>
+      <ul>
+        <li>
+          <strong>Desarrollador Full Stack</strong> - Navisoft Gaming (2022-2024)
+        </li>
+        <li>
+          <strong>Instructor TIC</strong> - SENA (2020-2022)
+        </li>
+        <li>
+          <strong>Desarrollador Front-End</strong> - IAS Software (2023)
+        </li>
+        <li>
+          <strong>Desarrollador Back-End</strong> - Ceiba Software (2024 - presente)
+        </li>
+      </ul>
+    </section>
+  );
+}
