66b30bcdb80a0b32bc34353be7aba68b7ed022bc
JulesPrz <julisperez.ch@gmail.com>
2025-11-20 16:13:15 -0500

    feat: componente CabeceraCV con datos personales actualizados

---
+++ src/components/CabeceraCV.jsx
@@ -0,0 +1,15 @@
+export default function CabeceraCV() {
+  return (
+    <header>
+      <h1>Juliana Pérez Chavarría</h1>
+      <h2>Desarrollador FullStack Jr.</h2>
+      <h3>
+        <ul>
+            <li>Celular: 3137369217</li>
+            <li>Correo: julisperez.ch@gmail.com</li>
+        </ul>
+        
+      </h3>
+    </header>
+  );
+}
