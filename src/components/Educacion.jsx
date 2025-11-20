333b69fe601b2305655adb5faa8a9377572b08cf
JulesPrz <julisperez.ch@gmail.com>
2025-11-20 16:13:45 -0500

    feat: componente Educacion con formación complementaria

---
+++ src/components/Educacion.jsx
@@ -0,0 +1,15 @@
+export default function Educacion() {
+  return (
+    <section>
+      <h3>Educación</h3>
+      <ul>
+        <li>
+          <strong>Tecnólogo en análisis y desarrollo de software</strong> - Sena (2018-2022)
+        </li>
+        <li>
+          <strong>Ingeniería en Sistemas</strong> - Universidad de Antioquia (2022 - presente)
+        </li>
+      </ul>
+    </section>
+  );
+}
